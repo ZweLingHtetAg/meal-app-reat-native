156e7af41bdf5642f484f82261250718b1eabfc3
Zwel Linn  Htet Ag <zwellinnhtetag@Zwels-MacBook.local>
2020-05-03 23:55:38 +0630

    Add Fonts

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchFont = () => {
+  Font.loadAsync({
+    'Josh':require('./assets/fonts/Jost-Regular.ttf'),
+    'josh-bold':require('./assets/fonts/Jost-Bold.ttf')
+  })
+};
 
 export default function App() {
+
+  const [fontLoad,setFontLoad] = useState(false);
+  
+  if(!fontLoad)
+  {
+    return <AppLoading startAsync={fetchFont} onFinish={()=>setFontLoad(true)} />;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Meals App is getting started</Text>
